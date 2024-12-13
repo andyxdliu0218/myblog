80a426a3e44d0264f99bfdca7b9893a416f9bc5f
andyxdliu0218 <andyxdliu0218@gmail.com>
2024-12-12 23:15:11 -0800

    Login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,30 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
+
+
 const Login = () =>{
 
-    return <div>this is Login</div>
+    return (
+        <div className='login'>
+            <Card className='login-container'>
+                <img className='login-logo' src={logo} alt="" />
+                <Form>
+                    <Form.Item>
+                        <Input size='large' placeholder='please enter your username'/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Input size='large' placeholder='please enter your password'/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type='primary' htmlType='submit' size='large' block>
+                            login
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
 }
 export default Login;
