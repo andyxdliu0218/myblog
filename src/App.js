f8fce76c33f99808a7544e6003458b3f504e0961
andyxdliu0218 <andyxdliu0218@gmail.com>
2024-12-12 22:38:20 -0800

    install sass,project strcture, install react-router-dom,install craco and config auto completion for '@' : src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import {Button} from 'antd'
+
 function App(){
-  return <div>this is APP</div>
+  return <div>this is APP <Button type='primary'> test</Button></div>
 }
 
 export default App;
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Layout from '@/pages/Layout' // src/pages/layout
+import Login from '@/pages/Login'
+
+
+
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout/>
+    },
+
+    {
+        path: "/login",
+        element: <Login/>
+    }
+])
+
+export default router
