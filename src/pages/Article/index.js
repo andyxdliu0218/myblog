e32754f1889ff1a5661a900ab9287f5cec37d1d2
andyxdliu0218 <andyxdliu0218@gmail.com>
2025-02-09 23:09:54 -0800

    create  Article page

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,5 +1,113 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  DatePicker,
+  Select,
+} from "antd";
+
+import { Table, Tag, Space } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import img404 from "@/assets/error.png";
+
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+
 const Article = () => {
-  return <div>this is article</div>;
+  const onFinish = (formValue) => {
+    console.log(formValue);
+    const { date, status } = formValue;
+
+    const data1 = `${date[0].year()}-${date[0].month() + 1}-${date[0].date()}`;
+    console.log(data1);
+  };
+  const columns = [
+    { title: "Title", dataIndex: "title" },
+    { title: "Read Count", dataIndex: "read_count" },
+    { title: "Comment Count", dataIndex: "comment_count" },
+    { title: "Like Count", dataIndex: "like_count" },
+    { title: "Publish Date", dataIndex: "pubdate" },
+    {
+      title: "Status",
+      dataIndex: "status",
+      render: (data) =>
+        data === 2 ? (
+          <Tag color="green">Approved</Tag>
+        ) : (
+          <Tag color="magenta">Pending review</Tag>
+        ),
+    },
+
+    {
+      title: "Action",
+      render: (data) => {
+        return (
+          <Space size="middle">
+            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+            <Button
+              type="primary"
+              danger
+              shape="circle"
+              icon={<DeleteOutlined />}
+            />
+          </Space>
+        );
+      },
+    },
+  ];
+
+  // body data
+  const data = [
+    {
+      id: "8218",
+      read_count: 2,
+      comment_count: 0,
+      like_count: 0,
+      pubdate: "2021-09-01 09:00:00",
+      status: 2,
+      title: "Article 1",
+    },
+  ];
+  return (
+    <div>
+      <Card
+        title={
+          <Breadcrumb
+            items={[
+              { title: <Link to={"/"}>Home</Link> },
+              { title: "Post Article" },
+            ]}
+          />
+        }
+        style={{ marginBottom: 20 }}
+      >
+        <Form initialValues={{ status: null }} onFinish={onFinish}>
+          <Form.Item label="Status" name="status">
+            <Radio.Group>
+              <Radio value={null}>All</Radio>
+              <Radio value={0}>Draft</Radio>
+              <Radio value={2}>Ok to Publish</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label="Date" name="date">
+            <RangePicker />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ marginleft: 40 }}>
+              Select Date
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+      <Card>
+        <Table rowKey="id" columns={columns} dataSource={data} />
+      </Card>
+    </div>
+  );
 };
 
 export default Article;
--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -3,22 +3,35 @@ import {
   Breadcrumb,
   Form,
   Button,
+  Upload,
   Radio,
   Input,
   Space,
   Select,
+  message,
 } from "antd";
-// import { PlusOutlined } from "@ant-design/icons";
+import { PlusOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import "./index.scss";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { createArticleAPI } from "@/apis/article";
+import { useState } from "react";
 
 const { Option } = Select;
 
 const Publish = () => {
   const [form] = Form.useForm();
+  // const [imageType, setImageType] = useState(0);
+
+  // const onChange = (e) => {
+  //   console.log(e.target.value);
+  //   setImageType(e.target.value);
+  // };
+
+  // const onFinish = (formValue) => {
+  //   console.log(formValue);
+  // };
 
   const onFinish = async (formValue) => {
     console.log(formValue);
@@ -31,6 +44,7 @@ const Publish = () => {
       return;
     } else {
       const res = await createArticleAPI(formValue);
+      message.success("Successfully Created Blog");
       console.log(res);
     }
   };
@@ -51,7 +65,7 @@ const Publish = () => {
           form={form}
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
-          initialValues={{ type: 1 }}
+          initialValues={{ type: 0 }}
           onFinish={onFinish}
         >
           <Form.Item
@@ -61,6 +75,24 @@ const Publish = () => {
           >
             <Input placeholder="Please input title" style={{ width: 400 }} />
           </Form.Item>
+
+          {/* <Form.Item label="Cover">
+            <Form.Item name="type">
+              <Radio.Group onChange={onChange}>
+                <Radio value={0}>Type 0</Radio>
+                <Radio value={1}>Type 1</Radio>
+                <Radio value={2}>Type 2</Radio>
+              </Radio.Group>
+            </Form.Item>
+
+            {imageType > 0 && (
+              <Upload listType="picture-card" showUploadList={true}>
+                <div style={{ marginTop: 8 }}>
+                  <PlusOutlined />
+                </div>
+              </Upload>
+            )}
+          </Form.Item> */}
 
           <Form.Item
             label="Content"
